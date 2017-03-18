c4099dc00b557f4cef5f6da6359c41ad8835000c
Kalin Gyokov <kalin_gyokov@hotmail.com>
2017-03-17 20:30:29 -0700

    Moving parser process into its own module (todo: move away from this pattern)

---
+++ src/mqttl_parse_proc.erl
@@ -0,0 +1,80 @@
+%%%-------------------------------------------------------------------
+%%% @author Kalin
+%%% @copyright (C) 2017, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 17. Mar 2017 8:14 PM
+%%%-------------------------------------------------------------------
+-module(mqttl_parse_proc).
+-author("Kalin").
+
+-include("mqttl_parsing.hrl").
+
+%% API
+-export([start_link/3]).
+
+%%
+%% @todo: Do not create a special process for parsing
+%%
+
+start_link(ConnPid,ConnMod,Opts) ->
+  {ok,spawn_link(fun() -> start_loop(ConnPid,ConnMod,Opts) end)}.
+
+start_loop(ConnPid,ConnMod, Opts) ->
+  BufferSize = proplists:get_value(buffer_size,Opts,128000),
+  %% calback for the parser process to get new data
+  ReadFun =
+    fun(ExpectedSize) ->
+      receive_data(ExpectedSize)
+    end,
+
+  ParseState = #parse_state{
+    buffer = <<>>,
+    max_buffer_size = BufferSize,
+    readfun =  ReadFun
+  },
+  loop_over_socket(ConnPid,ConnMod,ParseState).
+
+loop_over_socket(ConnPid,ConnMod,ParseState) ->
+  case mqttl_parser:parse_packet(ParseState) of
+    {ok, NewPacket,NewParseState} ->
+      error_logger:info_msg("processing packet ~p~n", [NewPacket]),
+      ConnMod:handle_packet(ConnPid,NewPacket),
+      loop_over_socket(ConnPid,ConnMod,NewParseState);
+    {error,Reason} ->
+      error_logger:info_msg("Parse error ~p~n", [Reason]),
+      handle_error(ConnPid,ConnMod,Reason)
+  end.
+
+handle_error(ConnPid, ConnMod, Reason) ->
+  case Reason of
+    invalid_flags ->
+      ConnMod:bad_packet(ConnPid,invalid_flags);
+    malformed_packet ->
+      ConnMod:bad_packet(ConnPid,undefined);
+    {unexpected_disconnect,Details} ->
+      ConnMod:unexpected_disconnect(ConnPid,Details)
+    %% @todo: More errors
+  end.
+
+%% callback for parser process
+receive_data(MinExpected) ->
+  receive_data(MinExpected,<<>>).
+
+receive_data(0,Acc) ->
+  receive
+    {data,Data} ->
+      {ok,<<Acc/binary,Data/binary>>};
+    _ -> exit(normal)
+  end;
+
+receive_data(MinExpected,Acc) when byte_size(Acc) >= MinExpected ->
+  {ok,Acc};
+
+receive_data(MinExpected,Acc) ->
+  receive
+    {data,Data} ->
+      receive_data(MinExpected,<<Acc/binary,Data/binary>>);
+    _ -> exit(normal)
+  end.
--- src/mqttl_receiver.erl
+++ src/mqttl_receiver.erl
@@ -138,14 +138,15 @@ handle_info({'EXIT',ParserPid, Reason},S = #state{conn_mod = ConnMod,
 handle_info({async_init,Ref,Opts},S = #state{transport = Transport,
                                              socket = Socket,
                                              conn_mod = ConnMod}) ->
-    {ok,ConnPid} = ConnMod:new(Transport,Socket,Opts),
+    {ok,ConnPid} = ConnMod:new_link(Transport,Socket,Opts),
     error_logger:info_msg("Connection Process ~p started",[ConnPid]),
     ok = ranch:accept_ack(Ref),
     ok = Transport:setopts(Socket, [{active, once}]),
-    ParserPid = spawn_link(fun() -> start_loop(ConnPid,ConnMod,Opts) end),
-    {noreply, S#state{conn_pid = ConnPid, parser_pid = ParserPid}};
+    {ok,ParserPid} = mqttl_parse_proc:start_link(ConnPid,ConnMod,Opts),
+    {noreply, S#state{conn_pid = ConnPid,
+                      parser_pid = ParserPid}};
 
-handle_info({tcp, Socket, Data}, S = #state{socket=Socket, transport=Transport, parser_pid = ParserPid}) ->
+handle_info({tcp, Socket, Data}, S = #state{socket = Socket, transport = Transport, parser_pid = ParserPid}) ->
     Transport:setopts(Socket, [{active, once}]),
     error_logger:info_msg("received data ~p",[Data]),
     ParserPid ! {data,Data},
@@ -201,65 +202,5 @@ code_change(_OldVsn, State, _Extra) ->
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
-
-
-start_loop(ConnPid,ConnMod, Opts) ->
-    %%TimeOut = proplists:get_value(read_timeout,Opts,30000),
-    BufferSize = proplists:get_value(buffer_size,Opts,128000),
-    %% calback for the parser process to get new data
-    ReadFun =
-        fun(ExpectedSize) ->
-            receive_data(ExpectedSize)
-        end,
-
-    ParseState = #parse_state{
-        buffer = <<>>,
-        max_buffer_size = BufferSize,
-        readfun =  ReadFun
-    },
-    loop_over_socket(ConnPid,ConnMod,ParseState).
-
-loop_over_socket(ConnPid,ConnMod,ParseState) ->
-    case mqttl_parser:parse_packet(ParseState) of
-        {ok, NewPacket,NewParseState} ->
-            error_logger:info_msg("processing packet ~p~n", [NewPacket]),
-            ConnMod:handle_packet(ConnPid,NewPacket),
-            loop_over_socket(ConnPid,ConnMod,NewParseState);
-        {error,Reason} ->
-            error_logger:info_msg("Parse error ~p~n", [Reason]),
-            handle_error(ConnPid,ConnMod,Reason)
-    end.
-
-handle_error(ConnPid, ConnMod, Reason) ->
-    case Reason of
-        invalid_flags ->
-            ConnMod:bad_packet(ConnPid,invalid_flags);
-        malformed_packet ->
-            ConnMod:bad_packet(ConnPid,undefined);
-        {unexpected_disconnect,Details} ->
-            ConnMod:unexpected_disconnect(ConnPid,Details)
-    %% @todo: More errors
-    end.
-
-%% callback for parser process
-receive_data(MinExpected) ->
-    receive_data(MinExpected,<<>>).
-
-receive_data(0,Acc) ->
-    receive
-        {data,Data} ->
-            {ok,<<Acc/binary,Data/binary>>};
-        _ -> exit(normal)
-    end;
-
-receive_data(MinExpected,Acc) when byte_size(Acc) >= MinExpected ->
-    {ok,Acc};
-
-receive_data(MinExpected,Acc) ->
-    receive
-        {data,Data} ->
-            receive_data(MinExpected,<<Acc/binary,Data/binary>>);
-        _ -> exit(normal)
-    end.
 
 
