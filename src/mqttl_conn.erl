422cbc47ce5b8cb8fe1cbc3555736fab342eac93
Kalin Gyokov <kalin_gyokov@hotmail.com>
2017-03-17 20:33:01 -0700

    Cleanup/comments

--- src/mqttl_conn.erl
+++ src/mqttl_conn.erl
@@ -11,12 +11,14 @@
 
 -include("mqttl_packets.hrl").
 
--type transport()::tcp | ssl.
-
 %% API
 -export([]).
 
--callback new_link(Transport::transport(),Socket:: any(),Opts::any()) -> {ok,pid()}.
+%% @doc
+%% Starts a new connection process, passing in a 'Send' module and a 'Send' reference
+%% (e.g. mqttl_ws and a Pid of a websocket process)
+%% @end
+-callback new_link(SendMod::module(),SendRef::any(),Opts::any()) -> {ok,pid()}.
 
 %%todo: Maybe just have the receiver quit and let the mqttl_conn process be killed?
 -callback unexpected_disconnect(Pid::any(),Details::any()) -> ok.
